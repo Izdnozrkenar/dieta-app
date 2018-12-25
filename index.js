d2e25a8090a0092514942667ae55f468fa0d38b8
DESKTOP-SAPIICD\Franciszek <fr.gorczyca@gmail.com>
2018-12-25 03:25:13 +0100

    proper query, added listener for updating DB (not finished tho)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
-
+const events = require('events')
 const mariadb = require('mariadb');
-const pool = mariadb.createPool({ host: 'localhost', port: '3306', user: 'root', password: '1wax2qsz', connectionLimit: 5 });
+
+var eventEmitterMain = new events.EventEmitter();
+
+const pool = mariadb.createPool({ host: 'localhost', port: '3306', user: 'root', password: '1wax2qsz', database: 'aplikacja_dieta' , connectionLimit: 5 });
 pool.getConnection()
     .then(conn => {
         console.log("connected ! connection id is " + conn.threadId);
@@ -10,6 +13,7 @@ pool.getConnection()
     .catch(err => {
         console.log("not connected due to error: " + err);
     });
+
 
 function product(prdId, prdEnergy, prdProtein, prdFat, prdCarbohydrates, prdFiber, prdName) {
     this.prdId = prdId;
@@ -27,13 +31,19 @@ function product(prdId, prdEnergy, prdProtein, prdFat, prdCarbohydrates, prdFibe
 };
 
 
-var productList = [];
+var dishList = [];
 
-pool.query('select * from aplikacja_dieta.products')
+var updateNutrientsInDatabase = function updateNutrientsInDatabase(){
+
+    console.log('udalo sie! przykladowe danie ' + dishList[4].totalEnergyAmount )
+}
+
+eventEmitterMain.addListener('dishes_received',updateNutrientsInDatabase);
+
+pool.query('SELECT dshName, (SUM(products_dishes_xref.prdAmount*products.prdEnergy)) AS \'totalEnergyAmount\', (SUM(products_dishes_xref.prdAmount*products.prdProtein)) AS \'totalProteinAmount\', (SUM(products_dishes_xref.prdAmount*products.prdFat)) AS \'totalFatAmount\', (SUM(products_dishes_xref.prdAmount*products.prdCarbohydrates)) AS \'totalCarbohydratesAmount\', (SUM(products_dishes_xref.prdAmount*products.prdFiber)) AS \'totalFiberAmount\' FROM dishes JOIN products_dishes_xref, products WHERE products_dishes_xref.dshID = dishes.dshID AND products.prdID = products_dishes_xref.prdID GROUP BY dishes.dshName')
     .then(res => {
-        res.forEach(element => {
-            productList.push(new product(element.prdID, element.prdEnergy, element.prdFat, element.prdCarbohydrates, element.prdFiber, element.prdName));
-    });
-    console.log(productList);
+        res.forEach(dish => {
+            dishList.push(dish)
+        });
+        eventEmitterMain.emit('dishes_received');
 });
-
