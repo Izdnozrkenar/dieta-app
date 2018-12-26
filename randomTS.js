467b83bdd9cb4d421aae46d0e1c8be05117f882d
DESKTOP-SAPIICD\Franciszek <fr.gorczyca@gmail.com>
2018-12-26 22:23:44 +0100

    added hoorbile quries to get dishes by type added separate random number generation

--- randomTS.js
+++ randomTS.js
@@ -1,24 +1,89 @@
 'use strict';
 const events = require('events');
+const randomNumber = require('./randomNumberGenerator');
+const crypto = require('crypto');
 
 var randomTabuEventEmitter = new events.EventEmitter();
 
-exports.generateRandomSolution = function (pool, conn) {
+var breakfastIdlist = [];
+var secondBreakfastIdlist = [];
+var lunchIdlist = [];
+var meriendaIdlist = [];
+var dinnerIdlist = [];
 
-   var dishIdlist = [];
+var solutionsTabuList = {};
 
-   randomTabuEventEmitter.addListener('dishesID_received', readIDs)
+var currentSolution = [];
+var bestSolution = [];
 
-   pool.query('SELECT dshID,dshType FROM dishes ORDER BY dshID')
+exports.generateRandomSolution = function (pool) {
+   
+   randomTabuEventEmitter.once('dishesID_received', generateFirstSolution)
+
+   getDishesByType(pool);
+}
+
+function getDishesByType(pool) {
+   pool.query('SELECT dshID,dshType FROM dishes WHERE dshType >= 10000')
       .then(res => {
          res.forEach(element => {
-            dishIdlist.push([element.dshID,element.dshType]);
+            breakfastIdlist.push(element.dshID);
+         });
+      })
+
+   pool.query('SELECT dshID FROM dishes WHERE ((dshType/1000)>=1 AND (FLOOR((dshType/1000))%2=1))')
+      .then(res => {
+         res.forEach(element => {
+            secondBreakfastIdlist.push(element.dshID);
+         });
+      })
+
+   pool.query('SELECT dshID FROM dishes WHERE ((dshType/100)>=1 AND (FLOOR((dshType/100))%2=1))')
+      .then(res => {
+         res.forEach(element => {
+            lunchIdlist.push(element.dshID);
+         });
+      })
+
+   pool.query('SELECT dshID FROM dishes WHERE ((dshType/10)>=1 AND (FLOOR((dshType/10))%2=1))')
+      .then(res => {
+         res.forEach(element => {
+            meriendaIdlist.push(element.dshID);
+         });
+      })
+
+   pool.query('SELECT dshID FROM dishes WHERE ((dshType/1)>=1 AND (FLOOR((dshType))%2=1))')
+      .then(res => {
+         res.forEach(element => {
+            dinnerIdlist.push(element.dshID);
          });
          randomTabuEventEmitter.emit('dishesID_received');
       })
+}
 
-   function readIDs() {
-      console.log(dishIdlist);
-      process.exit();
+function generateFirstSolution() {
+   for (var i = 0; i < 30; i++) {
+      var tempBreakfastId = randomNumber.getRandomNumber(0,breakfastIdlist.length-1);
+      var tempLunchId = randomNumber.getRandomNumber(0,lunchIdlist.length-1);
+      var tempDinnerId = randomNumber.getRandomNumber(0,lunchIdlist.length-1);
+
+      currentSolution.push([tempBreakfastId,null,tempLunchId,null,tempDinnerId]);
+   }  
+   var firstHash = crypto.createHash('md5').update(currentSolution.join()).digest('hex');
+
+   solutionsTabuList[firstHash]=10;
+   randomTabuSearch(solutionsTabuList(currentSolution));
+}
+
+function randomTabuSearch(solution){
+   for(var i=0; i<1; i++){
+
+      var temp1 = [randomNumber.getRandomNumber(0,4),randomNumber.getRandomNumber(0,29)];
+      var temp1 = [randomNumber.getRandomNumber(0,4),randomNumber.getRandomNumber(0,29)];
+      var temp1 = [randomNumber.getRandomNumber(0,4),randomNumber.getRandomNumber(0,29)];
+
+      console.log(temp1);
+      console.log(temp2);
+
    }
 }
