ed68ef4403bb03d03c1da46c0b8c06c8a28ef17a
DESKTOP-SAPIICD\Franciszek <fr.gorczyca@gmail.com>
2018-12-26 04:21:09 +0100

    finished diet evaluations, some humanity lost

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 const dbConnector = require('./poolmodule');
 const randomTS = require('./randomTS.js');
 const dbUpdate = require('./databaseStaticUpdate');
+const evaluator = require('./eval_condidtions')
 
 var dishList = [];
 
 const pool = dbConnector.setConnectionToDatabase();
 const conn = dbConnector.setCallbackConnectionToDatabase();
+
+var reqForUser = evaluator.calculateRequirements(2,78,1.9,63);
+console.log(evaluator.reqs);
 
 dbUpdate.updateDishesDatabase(pool,conn);
